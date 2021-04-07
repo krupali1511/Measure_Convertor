9951acf5dca15665d2945f985de76c9bb400173e
krupali1511 <knmehta0@gmail.com>
2021-04-07 23:23:02 +0530

    Convertor Added

---
+++ android/app/src/main/kotlin/com/krupali/measure_convertor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.krupali.measure_convertor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:measure_convertor/measure_conver.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Measure Converter',
+    home: MeasureConvertor(),
+  ));
+}
